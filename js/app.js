47515243aa9615cf84073cdf2c9a4799be772d26
Guillermo Machado <46054174+Guillermo07@users.noreply.github.com>
2019-03-21 13:57:24 -0600

    v.02 Calculadora
    Falta funcionalidad de punto y los caracteres de resultado igual a 8

--- js/app.js
+++ js/app.js
@@ -23,7 +23,9 @@ var division = document.getElementById("dividido");
 var multiplicacion = document.getElementById("por");
 var resta = document.getElementById("menos");
 var suma = document.getElementById("mas");
-
+var signo = document.getElementById("sign");
+var isSign = false;
+var isPunto = false;
 
 // Eventos
 
@@ -147,25 +149,145 @@ cero.onclick = function() {
 
 }
 
-
 resetear.onclick = function(e) {
     reset();
 
 }
+
 punto.onclick = function() {
-    resultado = resultado + ".";
-    if (resultado != "0") {
-        resultado = resultado + ".";
-    } else {
-        resultado =
-            resultado.slice(1);
+    resultado.innerHTML += ".";
+    if (resultado.innerHTML == "0") {
+        resultado.innerHTML += ".";
     }
 }
-punto.onclick = function(e) {
 
-    resultado.textContent = resultado.textContent + ".";
+sign.onclick = function() {
+    isSign = !isSign;
+
+    if (isSign && resultado.innerHTML > "0") {
+        resultado.innerHTML = "-" + resultado.innerHTML;
+    } else {
+        resultado.innerHTML =
+            resultado.innerHTML.slice(1);
+    }
 }
 
+// Listeners
+
+uno.addEventListener("mousedown", function() {
+    uno.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+uno.addEventListener("mouseout", function() {
+    uno.setAttribute("style", "transform:scale(1,1)")
+})
+dos.addEventListener("mousedown", function() {
+    dos.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+dos.addEventListener("mouseout", function() {
+    dos.setAttribute("style", "transform:scale(1,1)")
+})
+tres.addEventListener("mousedown", function() {
+    tres.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+tres.addEventListener("mouseout", function() {
+    tres.setAttribute("style", "transform:scale(1,1)")
+})
+cuatro.addEventListener("mousedown", function() {
+    cuatro.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+cuatro.addEventListener("mouseout", function() {
+    cuatro.setAttribute("style", "transform:scale(1,1)")
+})
+cinco.addEventListener("mousedown", function() {
+    cinco.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+cinco.addEventListener("mouseout", function() {
+    cinco.setAttribute("style", "transform:scale(1,1)")
+})
+seis.addEventListener("mousedown", function() {
+    seis.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+seis.addEventListener("mouseout", function() {
+    seis.setAttribute("style", "transform:scale(1,1)")
+})
+siete.addEventListener("mousedown", function() {
+    siete.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+siete.addEventListener("mouseout", function() {
+    siete.setAttribute("style", "transform:scale(1,1)")
+})
+ocho.addEventListener("mousedown", function() {
+    ocho.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+ocho.addEventListener("mouseout", function() {
+    ocho.setAttribute("style", "transform:scale(1,1)")
+})
+nueve.addEventListener("mousedown", function() {
+    nueve.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+nueve.addEventListener("mouseout", function() {
+    nueve.setAttribute("style", "transform:scale(1,1)")
+})
+cero.addEventListener("mousedown", function() {
+    cero.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+cero.addEventListener("mouseout", function() {
+    cero.setAttribute("style", "transform:scale(1,1)")
+})
+sign.addEventListener("mousedown", function() {
+    sign.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+sign.addEventListener("mouseout", function() {
+    sign.setAttribute("style", "transform:scale(1,1)")
+})
+raiz.addEventListener("mousedown", function() {
+    raiz.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+raiz.addEventListener("mouseout", function() {
+    raiz.setAttribute("style", "transform:scale(1,1)")
+})
+division.addEventListener("mousedown", function() {
+    division.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+division.addEventListener("mouseout", function() {
+    division.setAttribute("style", "transform:scale(1,1)")
+})
+multiplicacion.addEventListener("mousedown", function() {
+    multiplicacion.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+multiplicacion.addEventListener("mouseout", function() {
+    multiplicacion.setAttribute("style", "transform:scale(1,1)")
+})
+punto.addEventListener("mousedown", function() {
+    punto.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+punto.addEventListener("mouseout", function() {
+    punto.setAttribute("style", "transform:scale(1,1)")
+})
+resta.addEventListener("mousedown", function() {
+    resta.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+resta.addEventListener("mouseout", function() {
+    resta.setAttribute("style", "transform:scale(1,1)")
+})
+igual.addEventListener("mousedown", function() {
+    igual.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+igual.addEventListener("mouseout", function() {
+    igual.setAttribute("style", "transform:scale(1,1)")
+})
+suma.addEventListener("mousedown", function() {
+    suma.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+suma.addEventListener("mouseout", function() {
+    suma.setAttribute("style", "transform:scale(1,1)")
+})
+resetear.addEventListener("mousedown", function() {
+    resetear.setAttribute("style", "transform:scale(0.95,0.95)")
+})
+resetear.addEventListener("mouseout", function() {
+    resetear.setAttribute("style", "transform:scale(1,1)")
+})
 
 // Operaciones
 
@@ -226,5 +348,5 @@ function resolver() {
             break;
     }
     reset();
-    resultado.textContent = res;
+    resultado.innerHTML = res;
 }
